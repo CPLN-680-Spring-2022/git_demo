a9fd8cf341df743912d3b0adca2daaf5f274e1fe
jtannen <jonathan.tannen@gmail.com>
2022-01-30 10:18:44 -0500

    Updated for data-cleaning demo

---
+++ R/clean_data.R
@@ -0,0 +1,14 @@
+
+library(readr)
+library(dplyr)
+
+RAW_DATA_FOLDER <- "C:/Users/Jonathan Tannen/Dropbox/Documents/courses/git_demo/raw_data/"
+CLEANED_DATA_FOLDER <- "C:/Users/Jonathan Tannen/Dropbox/Documents/courses/git_demo/data/"
+
+clean_raw_data <- function(file){
+
+}
+
+if(FALSE){
+  clean_raw_data()
+}
---
+++ R/run_all.R
@@ -0,0 +1,12 @@
+
+source("clean_data.R")
+source("run_analysis.R")
+source("generate_output.R")
+
+RAW_DATA_FOLDER <- "../raw_data/"
+TEMP_FOLDER <- "tmp"
+OUTPUT_FOLDER <- "../outputs/"
+
+cleaned_data <- clean_and_save_data(RAW_DATA_FOLDER, out = OUTPUT_FOLDER)
+results <- run_analysis(cleaned_data)
+report <- generate_output(results)
