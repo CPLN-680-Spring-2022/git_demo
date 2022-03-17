7056ecb333665bf193d3b9150d97a74bde201c25
jtannen <jonathan.tannen@gmail.com>
2022-03-17 08:35:38 -0400

    clean data functions

--- R/clean_data.R
+++ R/clean_data.R
@@ -6,9 +6,10 @@ library(tidyr)
 RAW_DATA_FOLDER <- "C:/Users/Jonathan Tannen/Dropbox/Documents/courses/git_demo/raw_data/"
 CLEANED_DATA_FOLDER <- "C:/Users/Jonathan Tannen/Dropbox/Documents/courses/git_demo/data/"
 
+raw_data_path <- function(file) paste0(RAW_DATA_FOLDER, file)
 
 clean_raw_data <- function(file){
-  df <- read_csv(file)
+  df <- read_csv(raw_data_path(file))
   
   df$warddiv <- paste0(sprintf("%02d", df$WARD), "-", sprintf("%02d", df$DIVISION))
   df$type <- case_when(
@@ -24,7 +25,7 @@ clean_raw_data <- function(file){
 
 clean_raw_data_2020 <- function(file){
   warning("2020 does not have party info, party will be dropped.")
-  df <- read_csv(file)
+  df <- read_csv(raw_data_path(file))
   
   df <- df %>% pivot_longer(
     names_to="office_candidate", values_to="votes",
@@ -45,17 +46,26 @@ clean_raw_data_2020 <- function(file){
   return(df)
 }
 
+get_cleaning_func <- function(year){
+  if(year == "2020"){
+    return(clean_raw_data_2020)
+  } else{
+    return(clean_raw_data)
+  }
+}
+
 clean_and_save_raw_data <- function(){
-  all_files <- list.files(RAW_DATA_FOLDER, pattern = ".*\\.csv", full.names = T)
+  data_regex <- "([0-9]+)_([a-z]+).csv"
+  all_files <- list.files(RAW_DATA_FOLDER, pattern = data_regex)
+
   res <- list()
-  for(f in all_files){
-    year <- gsub(".*/([0-9]+)_[a-z]+.csv", "\\1", f)
-    election_type <- gsub(".*/[0-9]+_([a-z]+).csv", "\\1", f)
-    if(year == "2020"){
-      res[[f]] <- clean_raw_data_2020(f)
-    } else{
-      res[[f]] <- clean_raw_data(f)
-    }
+  for(file in all_files){
+    year <- gsub(data_regex, "\\1", file)
+    election_type <- gsub(data_regex, "\\2", file)
+    
+    cleaning_func <- get_cleaning_func(year)
+    res <- cleaning_func(file)
+    
     res[[f]]$year <- year
     res[[f]]$election_type <- election_type
   }
